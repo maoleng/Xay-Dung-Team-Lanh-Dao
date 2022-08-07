b6d14e68372c8ac6809c1baaa9b09b1bc7a8187b
Bùi Hữu Lộc <feature451@gmail.com>
2022-08-07 21:56:53 +0700

    model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'content', 'banner', 'likes', 'views',
+    ];
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'post_tag', 'post_id', 'tag_id');
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'post_user', 'post_id', 'user_id');
+    }
+
+
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'device'
+    ];
+
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class, 'post_user', 'user_id', 'post_id');
+    }
 }

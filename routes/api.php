744e9f8a09675fad352bf5246c8ab8d7974f2b29
Bùi Hữu Lộc <feature451@gmail.com>
2022-08-08 01:54:28 +0700

    seed posts (5)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Middleware\AuthApp;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'post', 'middleware' => AuthApp::class], static function() {
@@ -9,4 +10,10 @@ Route::group(['prefix' => 'post', 'middleware' => AuthApp::class], static functi
     Route::get('/{id}', [PostController::class, 'show']);
     Route::post('/', [PostController::class, 'store']);
     Route::post('/{id}', [PostController::class, 'like']);
+});
+Route::get('/test', function () {
+    $posts = Post::query()->get()[4]->content;
+    return view('welcome', [
+        'posts' => $posts
+    ]);
 });

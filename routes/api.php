96e775b6e901bfde73f611c328200ad6a462b0c6
Bùi Hữu Lộc <feature451@gmail.com>
2022-08-07 23:50:37 +0700

    beautiful route index

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Middleware\AuthApp;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'post', 'middleware' => AuthApp::class], static function() {
-    Route::get('/', [PostController::class, 'index']);
+    Route::get('/', [PostController::class, 'index'])->name('index');
     Route::get('/{id}', [PostController::class, 'show']);
     Route::post('/', [PostController::class, 'store']);
     Route::post('/{id}', [PostController::class, 'like']);
